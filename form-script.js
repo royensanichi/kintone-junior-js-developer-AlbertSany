dc8309b2b94d99a4998c28a2b2f8c46747807eff
kuya <aulya.khatulistivani@aqi.co.id>
2025-02-25 00:40:19 +0700

    feat: problems

--- form-script.js
+++ form-script.js
@@ -1,41 +1,10 @@
 (() => {
   const API_URL = 'http://localhost:3000/employees';
 
-  // Function to generate random string 10 digit alphanumerical
-  function generateRandomString(length = 10) {
-    const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-      result += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return result;
-  }
-
-  // Function to add new data
-  const form = document.getElementById('form');
-
-  console.log(form);
-
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    console.log('Form submitted');
-
-    const id = generateRandomString();
-    const name = document.querySelector('#name').value;
-    const position = document.querySelector('#position').value;
-
-    fetch(API_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({id, name, position}),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-
-        window.location.href = '/';
-      });
-  });
+  /**
+   * TODO:
+   * - Add a function to generate random string (10 digits alphanumeric) for id
+   * - Add a function to add new data (submit)
+   * - Add a function to redirect to the index page
+   */
 })();
--- index-script.js
+++ index-script.js
@@ -1,59 +1,12 @@
 (() => {
   const API_URL = 'http://localhost:3000/employees';
 
-  async function fetchData() {
-    return fetch(API_URL).then((response) => response.json());
-  }
-
-  function registerDeleteButton() {
-    const deleteButtons = document.querySelectorAll('.btn-delete');
-    deleteButtons.forEach((button) => {
-      button.addEventListener('click', async (event) => {
-        const id = event.target.getAttribute('data-id');
-        await fetch(`${API_URL}/${id}`, {
-          method: 'DELETE',
-        });
-
-        const data = await fetchData();
-        displayToTable(data);
-      });
-    });
-  }
-
-  function displayToTable(data) {
-    const tableBody = document.querySelector('tbody');
-    tableBody.innerHTML = '';
-    data.forEach((employee) => {
-      const tr = document.createElement('tr');
-      tr.innerHTML = `
-        <td>${employee.id}</td>
-        <td>${employee.name}</td>
-        <td>${employee.position}</td>
-        <td><button class="btn-delete" data-id="${employee.id}">delete</button></td>
-      `;
-      tableBody.appendChild(tr);
-    });
-
-    registerDeleteButton();
-  }
-
-  async function filterByName(event) {
-    const name = event.target.value;
-    const data = await fetchData();
-
-    const filteredData = data.filter((employee) => {
-      return employee.name.toLowerCase().includes(name.toLowerCase());
-    });
-
-    displayToTable(filteredData);
-  }
-
-  // Function to fetch and display data
-  document.addEventListener('DOMContentLoaded', async () => {
-    const data = await fetchData();
-
-    displayToTable(data);
-  });
-
-  document.getElementById('filter-name').addEventListener('input', filterByName);
+  /**
+   * TODO:
+   * - Add a function to redirect to the form page
+   * - Add a function to fetch data
+   * - Add a function to filter data by employee's name
+   * - Add a function to display table
+   * - Add a function to delete data (bonus)
+   */
 })();
